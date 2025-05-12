2fca9a25b3d4931531031398bcb1666be9118883
jhmin95 <tesla52@naver.com>
2025-05-12 18:45:35 +0900

    build: 필요한 모델만 다운로드받도록 설정. CV_TOKEN 추가

--- comfyui-without-flux/download_ALL.sh
+++ comfyui-without-flux/download_ALL.sh
@@ -3,13 +3,13 @@
 echo "Downloading all models"
 
 bash download_Files.sh
-bash download_Controlnet.sh
-bash download_LDSR.sh
-bash download_SUPIR.sh
-bash download_fp16.sh
-bash download_Florence-2.sh
-bash download_Upscalers.sh
-bash download_Outpainting.sh
-bash download_Workflows.sh
+# bash download_Controlnet.sh
+# bash download_LDSR.sh
+# bash download_SUPIR.sh
+# bash download_fp16.sh
+# bash download_Florence-2.sh
+# bash download_Upscalers.sh
+# bash download_Outpainting.sh
+# bash download_Workflows.sh
 
 echo "All models downloaded"
--- comfyui-without-flux/download_Files.sh
+++ comfyui-without-flux/download_Files.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
+# huggingface token
 if [[ -z "${HF_TOKEN}" ]] || [[ "${HF_TOKEN}" == "enter_your_huggingface_token_here" ]]
 then
     echo "HF_TOKEN is not set, can not download flux because it is a gated repository."
 else
     echo "HF_TOKEN is set, checking files..."
 
-    if [[ ! -e "/ComfyUI/models/vae/ae.sft" ]]
+    if [[ ! -e "/ComfyUI/models/checkpoints/FLUX1/flux1-dev-fp8.safetensors" ]]
     then
-        echo "Downloading ae.sft..."
-        wget -O "/ComfyUI/models/vae/ae.sft" --header="Authorization: Bearer ${HF_TOKEN}" "https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors?download=true"
+        echo "Downloading flux1-dev-fp8.sft..."
+        wget -O "/ComfyUI/models/checkpoints/FLUX1/flux1-dev-fp8.safetensors" --header="Authorization: Bearer ${HF_TOKEN}" "https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors?download=true"
     else
-        echo "ae.sft already exists, skipping download."
+        echo "flux1-dev-fp8.safetensors already exists, skipping download."
     fi
 
-    if [[ ! -e "/ComfyUI/models/diffusion_models/flux1-dev.sft" ]]
+    if [[ ! -e "/ComfyUI/models/controlnet/FLUX.1-dev-ControlNet-Union-Pro/diffusion_pytorch_model.safetensors" ]]
     then
-        echo "Downloading flux1-dev.sft..."
-        wget -O "/ComfyUI/models/diffusion_models/flux1-dev.sft" --header="Authorization: Bearer ${HF_TOKEN}" "https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors?download=true"
+        echo "Downloading diffusion_pytorch_model.safetensors..."
+        wget -O "/ComfyUI/models/controlnet/FLUX.1-dev-ControlNet-Union-Pro/diffusion_pytorch_model.safetensors" --header="Authorization: Bearer ${HF_TOKEN}" "https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors?download=true"
     else
-        echo "flux1-dev.sft already exists, skipping download."
+        echo "diffusion_pytorch_model.safetensors already exists, skipping download."
+    fi
+fi
+
+# civitai token
+if [[ -z "${CV_TOKEN}" ]] || [[ "${CV_TOKEN}" == "enter_your_civitai_token_here" ]]
+then
+    echo "CT_TOKEN is not set, can not download flux because it is a gated repository."
+else
+    echo "CT_TOKEN is set, checking files..."
+
+    if [[ ! -e "/ComfyUI/models/loras/RetroAnimeFluxV1.safetensors" ]]
+    then
+        echo "Downloading RetroAnimeFluxV1.safetensors.safetensors..."
+        wget -O "/ComfyUI/models/loras/RetroAnimeFluxV1.safetensors" "https://civitai.com/api/download/models/806265?token=${CV_TOKEN}"
+    else
+        echo "flux-realism-lora.safetensors already exists, skipping download."
     fi
 fi
 
@@ -38,8 +55,8 @@ download_file() {
 }
 
 # Download files
-download_file "/ComfyUI/models/clip" "clip_l.safetensors" "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors?download=true"
-download_file "/ComfyUI/models/clip" "t5xxl_fp8_e4m3fn.safetensors" "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors?download=true"
-download_file "/ComfyUI/models/loras" "GracePenelopeTargaryenV5.safetensors" "https://huggingface.co/WouterGlorieux/GracePenelopeTargaryenV5/resolve/main/GracePenelopeTargaryenV5.safetensors?download=true"
-download_file "/ComfyUI/models/loras" "VideoAditor_flux_realism_lora.safetensors" "https://huggingface.co/VideoAditor/Flux-Lora-Realism/resolve/main/flux_realism_lora.safetensors?download=true"
-download_file "/ComfyUI/models/xlabs/loras" "Xlabs-AI_flux-RealismLora.safetensors" "https://huggingface.co/XLabs-AI/flux-RealismLora/resolve/main/lora.safetensors?download=true"
+# download_file "/ComfyUI/models/clip" "clip_l.safetensors" "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors?download=true"
+# download_file "/ComfyUI/models/clip" "t5xxl_fp8_e4m3fn.safetensors" "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors?download=true"
+# download_file "/ComfyUI/models/loras" "GracePenelopeTargaryenV5.safetensors" "https://huggingface.co/WouterGlorieux/GracePenelopeTargaryenV5/resolve/main/GracePenelopeTargaryenV5.safetensors?download=true"
+# download_file "/ComfyUI/models/loras" "VideoAditor_flux_realism_lora.safetensors" "https://huggingface.co/VideoAditor/Flux-Lora-Realism/resolve/main/flux_realism_lora.safetensors?download=true"
+# download_file "/ComfyUI/models/xlabs/loras" "Xlabs-AI_flux-RealismLora.safetensors" "https://huggingface.co/XLabs-AI/flux-RealismLora/resolve/main/lora.safetensors?download=true"
